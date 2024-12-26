9cd90004f242e04a6075f61da4d97b150ed8ab21
Michal Haverlík <michal.haverlik@gmail.com>
2024-12-26 14:59:32 +0100

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { dbGetAll } from "@/lib/dbFunctions";
 import { getResponse } from "@/lib/completion";
 import { imageex } from "@/lib/image";
+import ImageUploadCard from "@/components/ui/upload";
 
 export default async function Home() {
-  // console.log(await dbGetAll("MestskePodnety", "Podnety"))
-  console.log((await getResponse(imageex)), 'ahoj')
+  //console.log(await dbGetAll("MestskePodnety", "Podnety"))
+  //console.log((await getResponse(imageex)), 'ahoj')
 
   return (
-    <div className="bg-black w-100 h-100">
-      aha
-    </div>
+    <main className="flex items-center justify-center min-h-screen bg-gray-50">
+      <ImageUploadCard />
+    </main>
   );
 }
---
+++ components/ui/uploadCardButtons.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function uploadCardButtons() {
+  const userAgent = navigator.userAgent;
+  const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
+
+  return (
+    <>
+      {/* Buttons */}
+      {!isMobile ? (
+        <Button
+          variant="outline"
+          onClick={() => document.getElementById("imageUpload")?.click()}
+        >
+          Choose Image
+        </Button>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <Button
+            variant="outline"
+            onClick={() => document.getElementById("cameraInput")?.click()}
+          >
+            Use Camera
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => document.getElementById("galleryInput")?.click()}
+          >
+            Choose from Gallery
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
