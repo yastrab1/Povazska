aaaf7b7bbbcfdd2aa4db68326ff263aae633e87a
Michal Haverlík <michal.haverlik@gmail.com>
2024-12-29 23:12:55 +0100

    newer openAI model

--- lib/completion.ts
+++ lib/completion.ts
@@ -1,13 +1,11 @@
 import { openai } from '@ai-sdk/openai';
 import { FilePart, generateObject, ImagePart, TextPart } from 'ai';
-import { Schema, z } from 'zod';
-import {strict} from "node:assert";
+import { z } from 'zod';
 
-const model = openai('gpt-4o-2024-08-06');
-// const default_schema = ;
+const model = openai('gpt-4o-2024-11-20');
 
-export async function getResponse(data: string[], schema: Schema = null) {
-  const prompt: (TextPart | ImagePart | FilePart)[] = [{ type: 'text', text: 'These images are a report of a problem in town summarize them in title of the problem (10 words max) and description of the problem (around 50 words max) and add tags, answer in slovak. Do not use tags that are not provided in your schema!' }];
+export async function getResponse(data: string[]) {
+  const prompt: (TextPart | ImagePart | FilePart)[] = [{ type: 'text', text: 'These images are a report of a problem in town summarize them in title of the problem (10 words max) and description of the problem (around 50 words max) and add tags, answer in slovak. ' }];
   const imagePrompt: ImagePart[] = data.map((value) => { return { type: 'image', image: new URL(value) }});
   prompt.push(...imagePrompt);
 
