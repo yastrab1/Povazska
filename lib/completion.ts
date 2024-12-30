488bb2b7d79c6896bc3b91f396babe7bbf591c4a
Michal Haverlík <michal.haverlik@gmail.com>
2024-12-30 20:10:37 +0100

    merge

--- lib/completion.ts
+++ lib/completion.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 const model = openai('gpt-4o-2024-11-20');
 
 export async function getResponse(data: string[]) {
-  const prompt: (TextPart | ImagePart | FilePart)[] = [{ type: 'text', text: 'These images are a report of a problem in town summarize them in title of the problem (10 words max) and description of the problem (around 50 words max) and add tags, answer in slovak. ' }];
+  const prompt: (TextPart | ImagePart | FilePart)[] = [{ type: 'text', text: 'These images are a report of a problem in town summarize them in title of the problem (10 words max) and description of the problem (around 50 words max) and add tags, answer in slovak.' }];
   const imagePrompt: ImagePart[] = data.map((value) => { return { type: 'image', image: new URL(value) }});
   prompt.push(...imagePrompt);
 
