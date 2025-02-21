e4ed0ceb22b42adae235486b073fdc46a23ff13e
yastrab <lipka.luki@gmail.com>
2025-02-21 11:44:11 +0100

    fixed x2,forgot to commit 1 line

--- app/api/resolve/route.ts
+++ app/api/resolve/route.ts
@@ -4,7 +4,7 @@ import {doc, updateDoc,} from "firebase/firestore";
 import {db} from "@/app/config/firebase";
 import {FilePart, generateObject, ImagePart, TextPart} from "ai";
 import {z} from "zod";
-import {Data, model} from "@/lib/globals";
+import {model} from "@/lib/globals";
 
 
 async function updateIssueDoc(issueID: string, resolve: string) {
@@ -19,13 +19,13 @@ export async function POST(
     const body = await req.json()
 
     const issueID = body["issueID"];
-    const issue = body["issueJSON"] as Data;
+    const issue = body["issueJSON"] as Issue;
     console.log(issue);
     console.log("called server action with id " + issueID);
     const prompt: (TextPart | ImagePart | FilePart)[] = [{
         type: 'text',
         text: `This is a description of a problem in town. Here are its pictures and its tags selected by the user. Suggest a way to resolve this issue for the Bratislava-Petržalka.
-        tags:${issue.userSelectedTags.join(",")}`
+        tags:${issue.tags.join(",")}`
     }];
     const imagePrompt: ImagePart[] = issue.images.map((value) => {
         return {type: 'image', image: new URL(value)}
